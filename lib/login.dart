a4dd288b3451a49f5134d75cc62d7bb6826543b9
mac <tchikh3@gmail.com>
2021-07-15 00:50:36 +0100

    add

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,7 @@ class _LoginState extends State<Login> {
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
   bool passvisible = true;
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,120 +21,137 @@ class _LoginState extends State<Login> {
         padding: const EdgeInsets.all(28.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'login',
-                  style: TextStyle(
-                    fontSize: 43.4,
-                    fontWeight: FontWeight.bold,
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'login',
+                    style: TextStyle(
+                      fontSize: 43.4,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 42.1,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (String value) {
-                    print(value); //click on submit
-                  },
-                  onChanged: (String value) {
-                    print(value); //one by one
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'email Address',
-                    labelStyle: TextStyle(color: Colors.greenAccent),
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.greenAccent,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
+                  SizedBox(
+                    height: 42.1,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    onFieldSubmitted: (String value) {
+                      print(value); //click on submit
+                    },
+                    onChanged: (String value) {
+                      print(value); //one by one
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'email add must nnot be empty';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'email Address',
+                      labelStyle: TextStyle(color: Colors.greenAccent),
+                      prefixIcon: Icon(
+                        Icons.email,
                         color: Colors.greenAccent,
-                        width: 2.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(
+                          color: Colors.greenAccent,
+                          width: 2.0,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 14.1,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: passvisible,
-                  onFieldSubmitted: (String value) {
-                    print(value); //click on submit
-                  },
-                  onChanged: (String value) {
-                    print(value); //one by one
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.greenAccent),
-                    prefixIcon: Icon(Icons.lock, color: Colors.greenAccent),
-                    suffixIcon: IconButton(
-                      color: Colors.black,
-                      icon: Icon(passvisible
-                          ? Icons.visibility
-                          : Icons.visibility_off),
+                  SizedBox(
+                    height: 14.1,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: passvisible,
+                    onFieldSubmitted: (String value) {
+                      print(value); //click on submit
+                    },
+                    onChanged: (String value) {
+                      print(value); //one by one
+                    },
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'pssword must nnot be empty';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.greenAccent),
+                      prefixIcon: Icon(Icons.lock, color: Colors.greenAccent),
+                      suffixIcon: IconButton(
+                        color: Colors.black,
+                        icon: Icon(passvisible
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            passvisible = !passvisible;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(
+                          color: Colors.greenAccent,
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22.0,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.greenAccent,
+                    child: MaterialButton(
                       onPressed: () {
-                        setState(() {
-                          passvisible = !passvisible;
-                        });
+                        if (formKey.currentState.validate()) {}
                       },
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
-                        color: Colors.greenAccent,
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 22.0,
-                ),
-                Container(
-                  width: double.infinity,
-                  color: Colors.greenAccent,
-                  child: MaterialButton(
-                    onPressed: () {},
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 11.1,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'dont have an account ?',
-                    ),
-                    TextButton(
-                      onPressed: () {},
                       child: Text(
-                        'Register Now',
-                        style: TextStyle(color: Colors.greenAccent),
+                        'LOGIN',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: 11.1,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'dont have an account ?',
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Register Now',
+                          style: TextStyle(color: Colors.greenAccent),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
